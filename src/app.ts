5b5ed8df39f96f3170fb26997a089db5d8e09b1d
Ivan Artemov <honyrik@gmail.com>
2021-11-10 20:15:54 +0300

    Updated template

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { EssenceCoreAuth } from './modules/EssenceCoreAuth';
 import { AppLogger } from './services/AppLogger';
+import './Constant';
 export async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true, logger: new AppLogger() });
 
